422cb13760865d4a5f4c3dd839edaf0c095fc70f
sunainaralia <124876937+sunainaralia@users.noreply.github.com>
2023-12-16 19:20:37 +0530

    oiuyh

--- mutualFund/settings.py
+++ mutualFund/settings.py
@@ -135,7 +135,7 @@ REST_FRAMEWORK = {
     ),
 }
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=365),  # Set to one year
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
