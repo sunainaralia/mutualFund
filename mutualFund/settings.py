fbd9d50ea78c1a227bded51f807538c00d75a9b7
sunainaralia <124876937+sunainaralia@users.noreply.github.com>
2023-12-25 16:06:10 +0530

    fejnf

--- mutualFund/settings.py
+++ mutualFund/settings.py
@@ -198,6 +198,6 @@ CHANNEL_LAYERS = {
 CORS_ALLOWED_ORIGINS = [
     "https://example.com",
     "https://sub.example.com",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:*",
+    "http://127.0.0.1:*",
 ]
